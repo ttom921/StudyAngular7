aea2d0877d009eabf1cf2a3401f1ce33c3c46717
ttom921 <ttom921@hotmail.com>
2019-04-22 14:52:14 +0800

    練習加入萬用路由

--- SimpleLayout/src/app/app-routing.module.ts
+++ SimpleLayout/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',children:[]},
+  {path:'p1',component:Page1Component},
+  {path:'p2',component:Page2Component},
+  {path:'p3',component:Page3Component},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
