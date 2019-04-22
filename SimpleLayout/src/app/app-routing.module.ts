914a1317eea3f2b71ae6e30692b0fa2b8cd67bcb
ttom921 <ttom921@hotmail.com>
2019-04-22 15:15:36 +0800

    加入404Page

--- SimpleLayout/src/app/app-routing.module.ts
+++ SimpleLayout/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
+import { Page404Component } from './page404/page404.component';
 
 const routes: Routes = [
   {path:'',children:[]},
   {path:'p1',component:Page1Component},
   {path:'p2',component:Page2Component},
   {path:'p3',component:Page3Component},
-  {path:'**',redirectTo:''}
+  {path:'404',component:Page404Component},
+  {path:'**',redirectTo:'404'}
+  //{path:'**',redirectTo:''}
+  //{path:'**',component:Page404Component}
 ];
 
 @NgModule({
